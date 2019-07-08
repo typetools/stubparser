3f7bb1c45cbcfdd0c0b8ffa62b76b283e1f95ed8
Michael Ernst <mernst@cs.washington.edu>
2019-07-08 14:30:32 -0700

    Remove unnecessary code

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -6,15 +6,6 @@ set -e
 
 export SHELLOPTS
 
-SLUGOWNER=${TRAVIS_PULL_REQUEST_SLUG%/*}
-if [[ "$SLUGOWNER" == "" ]]; then
-  SLUGOWNER=${TRAVIS_REPO_SLUG%/*}
-fi
-if [[ "$SLUGOWNER" == "" ]]; then
-  SLUGOWNER=typetools
-fi
-echo SLUGOWNER=$SLUGOWNER
-
 ## Compile
 echo "running \"mvn package\" for stubparser"
 cd javaparser-core && mvn -q package -Dmaven.test.skip=true
