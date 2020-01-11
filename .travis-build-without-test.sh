ffdca97900d4f7ba3392e5f782e0f90d3572a25e
Michael Ernst <mernst@cs.washington.edu>
2020-01-11 11:54:36 -0800

    Print Maven version

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -9,6 +9,7 @@ export SHELLOPTS
 
 ## Compile
 echo "running \"mvn package\" for stubparser"
+mvn --version
 cd javaparser-core && mvn -B -q package -Dmaven.test.skip=true
 
 echo Exiting "$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0")" in `pwd`
