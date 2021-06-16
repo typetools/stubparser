76737255e62a39051bbc449487b0622ab7984bdb
Michael Ernst <mernst@cs.washington.edu>
2021-06-16 10:04:23 -0700

    Take "travis" out of script names (#68)

---
+++ .build-without-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo Entering "$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0") in $(pwd)"
+
+# Fail the whole script if any command fails
+set -e
+
+export SHELLOPTS
+
+## Compile
+echo "running \"mvn package\" for stubparser"
+mvn --version
+# Try twice in case of network lossage
+(cd javaparser-core && mvn -B -q package -Dmaven.test.skip=true) \
+    || (sleep 2m && (cd javaparser-core && mvn -B -q dependency:resolve || true) \
+        && sleep 2m && (cd javaparser-core && mvn -B -q package -Dmaven.test.skip=true))
+
+echo Exiting "$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0") in $(pwd)"
