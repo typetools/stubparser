73ff9cbf0b4b425d9ccf40555c3f915f00a993c5
Michael Ernst <mernst@cs.washington.edu>
2021-07-12 11:40:53 -0700

    Use mvnw, not mvn

--- .build-without-test.sh
+++ .build-without-test.sh
@@ -11,8 +11,8 @@ export SHELLOPTS
 echo "running \"mvn package\" for stubparser"
 mvn --version
 # Try twice in case of network lossage
-(cd javaparser-core && mvn -B -q package -Dmaven.test.skip=true) \
-    || (sleep 2m && (cd javaparser-core && mvn -B -q dependency:resolve || true) \
-        && sleep 2m && (cd javaparser-core && mvn -B -q package -Dmaven.test.skip=true))
+(cd javaparser-core && ../mvnw -B -q package -Dmaven.test.skip=true) \
+    || (sleep 2m && (cd javaparser-core && ../mvnw -B -q dependency:resolve || true) \
+        && sleep 2m && (cd javaparser-core && ../mvnw -B -q package -Dmaven.test.skip=true))
 
 echo Exiting "$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0") in $(pwd)"
